226e619abe86701ad8487da445366f0934961aae
Mathias Carignani <mathcarignani@gmail.com>
2014-06-26 20:16:15 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Purasangre::Application.config.secret_key_base = '7424cbc30d3b09a4e8c8fc667facb237f570d76a9816e7df147ee607466ca93c0ba9846c68e3c8b8025219687572b235d9fa346c56b2e9eb278338c61170e3dd'
