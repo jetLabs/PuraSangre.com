bbcd0e14182e7d2e0e728cafeb1a7eb1bd634a08
Mathias Carignani <mathcarignani@gmail.com>
2014-07-04 20:50:37 -0300

    Seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Caballo.create(sexo: "Macho", record: "46-3", age: 4, nombre: "Terminator", precio: 40000.0, imagen: "caballo1.jpg")
+Caballo.create(sexo: "Hembra", record: "0-10", age: 2, nombre: "La pepa", precio: 3500.0, imagen: "caballo2.jpg")
+Caballo.create(sexo: "Macho", record: "20-2", age: 3, nombre: "El incontrolable", precio: 10000.0, imagen: "caballo3.jpg")
