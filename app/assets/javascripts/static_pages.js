8792e03a1e72fd986fac51c7b13ab27354ef72ff
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-07-04 17:22:30 -0300

    Background image changed, Blur fixed

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,8 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$('#wrapper-landing').blurjs({
-	source: 'body',
-	radius: 10,
-	overlay: 'rgba(0,0,0,0.8)'
-});
+$( document ).ready(function() {
+	$('body').addClass( "bg-landing" );
+        $('.bg-landing').blurjs({
+			source: 'body',
+			radius: 7,
+			overlay: 'rgba(0,0,0,0.8)'
+		});
+ });
