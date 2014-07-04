609b16d2d421cb870c4aad5370012617eb0b3aaa
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-07-04 16:14:52 -0300

    Application layout fixed

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$('#wrapper-landing').blurjs({
+	source: 'body',
+	radius: 10,
+	overlay: 'rgba(0,0,0,0.8)'
+});
